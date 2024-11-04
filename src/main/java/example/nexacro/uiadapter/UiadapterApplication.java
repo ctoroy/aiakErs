b1722a38ae44136b231e15247cf525d45faa4697
roy <thpark1123@gmail.com>
2024-11-04 16:34:27 +0900

    maven pom -> gradle 변경

--- src/main/java/example/nexacro/uiadapter/UiadapterApplication.java
+++ src/main/java/example/nexacro/uiadapter/UiadapterApplication.java
@@ -2,10 +2,11 @@ package example.nexacro.uiadapter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 // 일반 실행 형식
 public class UiadapterApplication {
 
